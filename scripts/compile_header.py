63104a7de7c5cc6d9db598677366a94541f047d4
Francois Chabot <francois.chabot.dev@gmail.com>
2017-09-27 23:04:14 -0400

    Header compilation script now centralizes the copyright notice.

--- scripts/compile_header.py
+++ scripts/compile_header.py
@@ -9,6 +9,12 @@ args = parser.parse_args()
 
 loaded_files = set()
 
+copyright_notice = '''//  Copyright 2017 Francois Chabot
+//  (francois.chabot.dev@gmail.com)
+//
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)'''
 
 def process(file, dst):
   if file in loaded_files:
@@ -19,7 +25,7 @@ def process(file, dst):
   with open(file) as f:
     for line in f:
       stripped_line = line.strip()
-      if not stripped_line:
+      if not stripped_line or stripped_line.startswith(r'//'):
         continue
       if stripped_line.startswith("#include"):
         path = stripped_line[8:].strip()
@@ -31,4 +37,5 @@ def process(file, dst):
 
 
 with open(args.output, "w") as destination:
+  print(copyright_notice, file=destination)
   process(args.input, destination)
