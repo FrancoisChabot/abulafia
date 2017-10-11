66d2ffc27c6ee329a6059dd3ab09943c3e3abd2a
Francois Chabot <francois.chabot.dev@gmail.com>
2017-10-11 02:48:52 -0400

    Added a few interesting recursive test cases

--- tests/patterns/test_recur.cpp
+++ tests/patterns/test_recur.cpp
@@ -35,6 +35,92 @@ TEST(test_recur, recur_test) {
   testPatternSuccess("123456", recur, nil);
 }
 
+
+TEST(test_recur, node_generation) {
+  RecurMemoryPool pool;
+
+  struct Node {
+    char c;
+    std::vector<Node> n;
+
+    bool operator==(Node const& rhs) const {
+      return c == rhs.c && n == rhs.n;
+    }
+  };
+  
+  auto make_node = [](std::tuple<char, std::vector<Node>> v) { 
+    return Node{std::get<0>(v), std::get<1>(v)}; 
+  };
+
+  Recur<struct abc, Node> recur(pool);
+
+  auto recur_pat = ( char_() >> '(' >> *recur >> ')' )[make_node];
+  
+  ABU_Recur_define(recur, abc, recur_pat);
+  
+  Node expected;
+  expected.c = 'a';
+  expected.n.resize(2);
+  expected.n[0].c = 'b';
+  expected.n[1].c = 'c';
+
+  testPatternSuccess("a(b()c())", recur, expected);
+}
+
+TEST(test_recur, multi_recur_test) {
+  RecurMemoryPool pool;
+
+  struct SubNode {
+    unsigned int c;
+    std::vector<SubNode> s;
+
+    bool operator==(SubNode const& rhs) const {
+      return c == rhs.c && s == rhs.s;
+    }
+  };
+
+  struct Node {
+    unsigned int c;    
+    std::vector<Node> n;
+    std::vector<SubNode> s;
+
+    bool operator==(Node const& rhs) const {
+      return c == rhs.c && s == rhs.s && n == rhs.n;
+    }
+  };
+
+  auto make_node = [](std::tuple<unsigned int, std::vector<Node>, std::vector<SubNode>> p) { 
+    return Node{std::get<0>(p), std::get<1>(p), std::get<2>(p)}; 
+  };
+
+  auto make_sub_node = [](std::tuple<unsigned int, std::vector<SubNode>> p) { 
+    return SubNode{std::get<0>(p), std::get<1>(p)}; 
+  };
+
+  Recur<struct abc, Node> recur(pool);
+  Recur<struct def, SubNode> sub_recur(pool);
+ 
+  auto sub_recur_pat = ( uint_ >> '(' >> *sub_recur >> ')' )[make_sub_node];
+  auto recur_pat = ( uint_ >> '(' >> *recur >> ')' >> '[' >> *sub_recur >> ']' )[make_node];
+  
+  ABU_Recur_define(recur, abc, recur_pat);
+  ABU_Recur_define(sub_recur, def, sub_recur_pat);
+  
+  Node expected;
+
+  expected.c = 1;
+  expected.s.resize(2);
+  expected.s[0].c = 2;
+  expected.s[1].c = 3;
+  expected.s[1].s.resize(1);
+  expected.s[1].s[0].c = 4;
+
+  testPatternSuccess("1()[2()3(4())]", recur, expected);
+
+}
+
+
+
 // Reproduces https://github.com/FrancoisChabot/abulafia/issues/17
 template <typename PAT_T>
 auto as_recur(PAT_T const& p, RecurMemoryPool& pool) {
